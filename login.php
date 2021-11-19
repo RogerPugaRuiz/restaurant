7e33276c689fe947f604b88485c3cdc61c630a39
Roger Puga Ruiz <roger@pop-os.localdomain>
2021-11-19 09:36:29 +0100

    message error

--- login.php
+++ login.php
@@ -23,46 +23,46 @@
     require_once "fn-php/check_login.php";
     require_once "message.php";
 
+    $message_error = "";
+    $username = "";
+    $password = "";
+
     if (filter_has_var(INPUT_POST, "loginsubmit")) {
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
         $user = login($username, $password);
         if (!$user) {
-            // getMessage("Login failed", "user name or password is incorrect");
+            $message_error = "Username or password incorrect";
         }else{
             $_SESSION["name"] = $user->getUsername();
             $_SESSION["password"] = $user->getPassword();
-            // echo $_SESSION["name"]. " -- " .$_SESSION["password"];
+            
             header("Location:index.php");
         }
-
-
-        // if (isset($user) && $user instanceof User) {
-        //     $_SESSION["name"] = $user->getUsername();
-        //     $_SESSION["password"] = $user->getPassword();
-        // } else {
-        //     // getMessage("Login failed", "user name or password is incorrect");
-        // }
     }
-
+    if (filter_has_var(INPUT_POST, "cancel")) {
+        header("Location: index.php");
+    }
     ?>
     <div class="container-fluid">
         <h2>Login form</h2>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <div class="form-group">
                 <label for="username">User Name:</label>
-                <input type="username" class="form-control" id="username" placeholder="Enter username" name="username">
+                <input type="username" class="form-control" id="username" placeholder="Enter username" name="username" value="<?php echo $username ?>">
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+                <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" value="<?php echo $password ?>">
             </div>
             <div class="checkbox">
                 <label><input type="checkbox" name="remember"> Remember me</label>
             </div>
             <button type="submit" name="loginsubmit" class="btn btn-default">Submit</button>
+            <button name="cancel" class="btn btn-default">Cancel</button>
         </form>
+        <p style="color:red"><?php echo $message_error ?></p>
     </div>
     <script src="js/close_message.js"></script>
 </body>
